3b746425a599a7b416cf11377ab3f36628d73887
Yena Park <yenapark@nyu.edu>
2019-10-27 17:06:12 -0400

    made react app to display now playing

--- web-api-auth-examples/client_credentials/app.js
+++ web-api-auth-examples/client_credentials/app.js
@@ -7,37 +7,45 @@
  * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
  */
 
-var request = require('request'); // "Request" library
+// var request = require('request'); // "Request" library
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
+// var client_id = 'CLIENT_ID'; // Your client id
+// var client_secret = 'CLIENT_SECRET'; // Your secret
 
-// your application requests authorization
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
+// // your application requests authorization
+// var authOptions = {
+//   url: 'https://accounts.spotify.com/api/token',
+//   headers: {
+//     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+//   },
+//   form: {
+//     grant_type: 'client_credentials'
+//   },
+//   json: true
+// };
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
 
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/jmperezperez',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
-    });
-  }
-});
+//     // use the access token to access the Spotify Web API
+//     var token = body.access_token;
+//     var options = {
+//       url: 'https://api.spotify.com/v1/users/jmperezperez',
+//       headers: {
+//         'Authorization': 'Bearer ' + token
+//       },
+//       json: true
+//     };
+//     request.get(options, function(error, response, body) {
+//       console.log(body);
+//     });
+//   }
+// });
+
+render() {
+  return (
+    <div className='App'>
+      <a href='http://localhost:8888'> Login to Spotify </a>
+    </div>
+  )
+}
