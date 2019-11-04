f471f687900b7f96bdda16792d30a26a03676f2e
Yena Park <yenapark@nyu.edu>
2019-11-04 12:50:13 -0500

    added routes for login and profile pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import './index.css';
-import App from './App';
+
+import App from './components/App';
+import Profile from './components/Profile'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routes = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">App</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={() => (<Redirect to='/app' />)} />
+      <Route exact path="/app" component={App} />
+      <Route path="/profile" component={Profile} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
