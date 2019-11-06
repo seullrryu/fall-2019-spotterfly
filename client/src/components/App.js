3337fb9183fe9bc63fff1703f9762a80a40f91aa
SZhongheng <zs967@nyu.edu>
2019-11-06 12:50:24 -0500

    what

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login.js";
+import Profile from "./components/Profile.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <section class="login">
-          <h1>SPOTTERFLY</h1>
-          <p1>Share your playlists with people near you with similar tastes.</p1>
-          <p>Discover new music.</p>
-          <a href='http://localhost:8888/login'>
-            <button id="login-button">
-              <b>LOG IN WITH SPOTIFY</b>
-            </button>
-          </a>
-        </section>
-        <footer>Copyright © Seulmin Ryu, Yena Park, Alexander Goldman, Zhongheng Sun 2019</footer>
-      </div>
+      <router>
+        <div className="App">
+          <Route path="/Login" Component={Login} />
+          <Route path="/Profile" Component={Profile} />
+
+          <footer>
+            Copyright © Seulmin Ryu, Yena Park, Alexander Goldman, Zhongheng Sun
+            2019
+          </footer>
+        </div>
+      </router>
     );
   }
 }
 
-export default App;  
+export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import "../App.css";
 
 class Login extends Component {
-    render() {
-        return(
-            <section class="login">
-                <h1>SPOTTERFLY</h1>
-                <p1>Share your playlists with people near you with similar tastes.</p1>
-                <p>Discover new music.</p>
-                <a href='http://localhost:8888/login'>
-                    <button id="login-button">
-                    <b>LOG IN WITH SPOTIFY</b>
-                    </button>
-                </a>
-            </section>
-        ) 
-    }
+  render() {
+    return (
+      <section class="login">
+        <h1>SPOTTERFLY</h1>
+        <p1>Share your playlists with people near you with similar tastes.</p1>
+        <p>Discover new music.</p>
+        <a href="http://localhost:8888/login">
+          <button id="login-button">
+            <b>LOG IN WITH SPOTIFY</b>
+          </button>
+        </a>
+      </section>
+    );
+  }
 }
 export default Login;
