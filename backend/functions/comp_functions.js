5a909dfcab427c446f85a0d279d1fd9e205aa8ad
SZhongheng <zs967@nyu.edu>
2019-11-25 14:03:12 -0500

    some changes

--- backend/functions/comp_functions.js
+++ backend/functions/comp_functions.js
@@ -1,89 +1,84 @@
-const MyFunctions = require('./MyFunctions');
+const MyFunctions = require("./MyFunctions");
 
-
-
-function DisplayOverlap(user1,user2,overlap){ // returns array of non overlapping songs 
-    var screenOut = [];
-    for(var i = 0; i < user1.length; i++ ){
-        if (user2.indexOf(user1[i]) === -1){
-            if(overlap){
-                screenOut.push(user2[i]); //overlaps 
-            }
-        } else {
-            if(!overlap){
-                screenOut.push(user2[i]); //non overlaps 
-            }
-        }
-    } 
-    return screenOut;// non overlapping songs 
+function DisplayOverlap(user1, user2, overlap) {
+  // returns array of non overlapping songs
+  var screenOut = [];
+  for (var i = 0; i < user1.length; i++) {
+    if (user2.indexOf(user1[i]) === -1) {
+      if (overlap) {
+        screenOut.push(user2[i]); //overlaps
+      }
+    } else {
+      if (!overlap) {
+        screenOut.push(user2[i]); //non overlaps
+      }
+    }
+  }
+  return screenOut; // non overlapping songs
 }
 
-
-function OverlapCheck(user1,user2,overlap){ // user1 and user2 are song arrays overlap is how many songs they need in common to match 
-    var count;
-    for(var i =0; i < user1.length; i++ ){
-        if (user2.indexOf(user1[i]) !== -1){
-            count ++; 
-        }
-    } if (count >= overlap){
-        var screenOut = DisplayOverlap(user1,user2,true); //checks for overlap
-        if (screenOut === undefined || screenOut.length == 0) {
-            return false;
-        } else {
-            var extras = DisplayOverlap(user1,user2,false); // gets list of all non overlaped 
-            //send var extras and screenOut to front end.  or figure out how to do this 
-            //screen out is what they matched on 
-            //extras are the ones that they differed 
-
-
-
-
-            
-            return true;
-        }
+function OverlapCheck(user1, user2, overlap) {
+  // user1 and user2 are song arrays overlap is how many songs they need in common to match
+  var count;
+  for (var i = 0; i < user1.length; i++) {
+    if (user2.indexOf(user1[i]) !== -1) {
+      count++;
     }
-    return false; 
+  }
+  if (count >= overlap) {
+    var screenOut = DisplayOverlap(user1, user2, true); //checks for overlap
+    if (screenOut === undefined || screenOut.length == 0) {
+      return false;
+    } else {
+      var extras = DisplayOverlap(user1, user2, false); // gets list of all non overlaped
+      //send var extras and screenOut to front end.  or figure out how to do this
+      //screen out is what they matched on
+      //extras are the ones that they differed
 
-
-    function distance(lat1, lon1, lat2, lon2) { //geolocation obeject me 
-        if ((lat1 == lat2) && (lon1 == lon2)) {
-            return 0;
-        }
-        else {
-            var newlat1 = Math.PI * lat1/180;
-            var newlat2 = Math.PI * lat2/180;
-            var theta = lon1-lon2;
-            var newtheta = Math.PI * theta/180;
-            var dist = Math.sin(newlat1) * Math.sin(newlat2) + Math.cos(newlat1) * Math.cos(newlat2) * Math.cos(newtheta);
-            if (dist > 1) {
-                dist = 1;
-            }
-            dist = Math.acos(dist);
-            dist = dist * 180/Math.PI;
-            dist = dist * 60 * 1.1515;
-    
-        }
-        return dist;
+      return true;
     }
+  }
+  return false;
 
-    function distance_checker(dist,range){ // takes in distance from formula and checks it with range 
-        if(Math.abs(dist) <= range){
-            return true;
-        } else {
-            return false;
-        }
+  function distance(lat1, lon1, lat2, lon2) {
+    //geolocation obeject me
+    if (lat1 == lat2 && lon1 == lon2) {
+      return 0;
+    } else {
+      var newlat1 = (Math.PI * lat1) / 180;
+      var newlat2 = (Math.PI * lat2) / 180;
+      var theta = lon1 - lon2;
+      var newtheta = (Math.PI * theta) / 180;
+      var dist =
+        Math.sin(newlat1) * Math.sin(newlat2) +
+        Math.cos(newlat1) * Math.cos(newlat2) * Math.cos(newtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = (dist * 180) / Math.PI;
+      dist = dist * 60 * 1.1515;
     }
+    return dist;
+  }
 
+  function distance_checker(dist, range) {
+    // takes in distance from formula and checks it with range
+    if (Math.abs(dist) <= range) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-
-// function checkAll(currentUser, otherUsers) needs to be made 
-//must determine how to loop through all users to grab songs 
-//takes in current user as an object and looks at its location first 
-// if the location matches with the otherUser location by calling myFunctions distance_checker 
+// function checkAll(currentUser, otherUsers) needs to be made
+//must determine how to loop through all users to grab songs
+//takes in current user as an object and looks at its location first
+// if the location matches with the otherUser location by calling myFunctions distance_checker
 // call overlapCheck() on user1's playlist and the user that has been flagged playlist.
-// must shoot the list returned back out to frontend, or store in a dictionary 
-// dictionary in terms of {User: {overlaps:[], nOverlaps:[]}} and return that dictionary 
-// will see professor for this 
+// must shoot the list returned back out to frontend, or store in a dictionary
+// dictionary in terms of {User: {overlaps:[], nOverlaps:[]}} and return that dictionary
+// will see professor for this
 
-    module.exports = {OverlapCheck, DisplayOverlap}
+//  module.exports = {OverlapCheck, DisplayOverlap}
--- backend/model/userData.model.js
+++ backend/model/userData.model.js
@@ -1,39 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userDataSchema = new Schema({
+const userDataSchema = new Schema(
+  {
     UserID: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    }, 
-    Username: {
-        type: String,
-        required: true,
-        trim: true,
-    },   //need to add location type GeoLocation Object 
-
-        //make dictionary of artist / song combos 
-    Song: {
-        type: [],
-        trim: true,
-    }, 
-    longitude: {
-        type: Float64Array,
-        //required: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
     },
-    latitude: { 
-        type: Float64Array,
-        //required: true,
-        trim: true,
-    }
-}, {
-    timestamps: true, // use some sort of time for login / sync not this one 
-});
+    Username: {
+      type: String,
+      required: true,
+      trim: true
+    }, //need to add location type GeoLocation Object
 
-const UserData = mongoose.model('userData', userDataSchema);
+    //make dictionary of artist / song combos
+    Song: {
+      type: [],
+      trim: true
+    },
+    longitude: {
+      //     type: Float64Array,
+      //required: true,
+      //    trim: true
+    },
+    latitude: {
+      //    type: Float64Array,
+      //required: true,
+      //    trim: true
+    }
+  },
+  {
+    timestamps: true // use some sort of time for login / sync not this one
+  }
+);
+
+const UserData = mongoose.model("userData", userDataSchema);
 
 module.exports = UserData;
