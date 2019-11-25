6efc663590262bf60a1b738c13a3d931c5b8d2bd
Seulmin Ryu <sr4632@nyu.edu>
2019-11-25 15:57:27 -0500

    will work now

--- client/src/components/Artists.js
+++ client/src/components/Artists.js
@@ -4,27 +4,29 @@ import "../App.scss";
 import axios from "axios";
 //import SpotifyWebApi from "spotify-web-api-js";
 //const spotifyApi = new SpotifyWebApi();
-function ArtistItem(props) {
-  const name = props.obj;
-  const pics = props.pics;
-  var index = props.index;
-  return (
-    <li>
-      <div className="artist-item">
-        <span>{name}</span>
-        <br></br>
-        <img
-          src={pics[index]}
-          width="300"
-          height="300"
-          alt="We don't have the images. Sorry :("
-        />
-        <br></br>
-        <br></br>
-      </div>
-    </li>
-  );
-}
+
+
+// function ArtistItem(props) {
+//   const name = props.obj;
+//   const pics = props.pics;
+//   var index = props.index;
+//   return (
+//     <li>
+//       <div className="artist-item">
+//         <span>{name}</span>
+//         <br></br>
+//         <img
+//           src={pics[index]}
+//           width="300"
+//           height="300"
+//           alt="We don't have the images. Sorry :("
+//         />
+//         <br></br>
+//         <br></br>
+//       </div>
+//     </li>
+//   );
+// }
 
 function TracksItem(props) {
   const name = props.obj;
