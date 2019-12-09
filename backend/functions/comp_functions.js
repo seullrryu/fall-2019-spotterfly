de21ef484271723059b482af9c6666c033cb9a8f
Seulmin Ryu <sr4632@nyu.edu>
2019-12-09 02:02:28 -0500

    printed user data on to the friends page, but we need to get the song names now, right now it's printing the songIDs

--- backend/functions/comp_functions.js
+++ backend/functions/comp_functions.js
@@ -4,10 +4,10 @@ function OverlapCheck(user1,user2,overlap){ // user1 and user2 are song arrays o
   var count = 0;
   var u2Songs = user2.Songs;
   var screenOut = [];
-  for(var i =0; i < user1.length; i++ ){
+  for (var i =0; i < user1.length; i++ ){
     if (u2Songs.indexOf(user1[i]) !== -1){
       screenOut.push(user1[i]);
-      count ++; 
+      count++; 
     }
   } if (count >= overlap){
     return screenOut        
@@ -15,38 +15,36 @@ function OverlapCheck(user1,user2,overlap){ // user1 and user2 are song arrays o
     return null;
   }
 }
-  
 
-
-  function distance(lat1, lon1, lat2, lon2) {
-    if (lat1 === lat2 && lon1 === lon2) {
-      return 0;
-    } else {
-      var newlat1 = (Math.PI * lat1) / 180;
-      var newlat2 = (Math.PI * lat2) / 180;
-      var theta = lon1 - lon2;
-      var newtheta = (Math.PI * theta) / 180;
-      var dist =
-        Math.sin(newlat1) * Math.sin(newlat2) +
-        Math.cos(newlat1) * Math.cos(newlat2) * Math.cos(newtheta);
-      if (dist > 1) {
-        dist = 1;
-      }
-      dist = Math.acos(dist);
-      dist = (dist * 180) / Math.PI;
-      dist = dist * 60 * 1.1515;
+function distance(lat1, lon1, lat2, lon2) {
+  if (lat1 === lat2 && lon1 === lon2) {
+    return 0;
+  } else {
+    var newlat1 = (Math.PI * lat1) / 180;
+    var newlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var newtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(newlat1) * Math.sin(newlat2) +
+      Math.cos(newlat1) * Math.cos(newlat2) * Math.cos(newtheta);
+    if (dist > 1) {
+      dist = 1;
     }
-    return dist;
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
   }
+  return dist;
+}
 
-  function distance_checker(dist, range) {
-    // takes in distance from formula and checks it with range
-    if (Math.abs(dist) <= range) {
-      return true;
-    } else {
-      return false;
-    }
+function distance_checker(dist, range) {
+  // takes in distance from formula and checks it with range
+  if (Math.abs(dist) <= range) {
+    return true;
+  } else {
+    return false;
   }
+}
 
 
 function checkAll(user1){
@@ -56,7 +54,7 @@ function checkAll(user1){
         continue;
       }
       var matched_songs = []
-      if(dist(distance_checker(user1.LonLat[0],user1.LonLat[1],myDoc.LonLat[0],myDoc.LonLat[1]),5)){ // takes distances and checks through distance to see if its in range.#TODO change the range 
+      if (dist(distance_checker(user1.LonLat[0],user1.LonLat[1],myDoc.LonLat[0],myDoc.LonLat[1]),5)) { // takes distances and checks through distance to see if its in range.#TODO change the range 
         console.log("passed distance check");
         if ((matched_songs = OverlapCheck(user1.Songs, myDoc,1))=== null) { // idk if the matched_songs = in an if works but it should
           console.log("No matches!!!!!");
@@ -69,4 +67,4 @@ function checkAll(user1){
 }
 
 
-    module.exports = checkAll;
+module.exports = checkAll;
